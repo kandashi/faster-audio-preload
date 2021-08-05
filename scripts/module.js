01996183aa0a0df3167e2028c20f046022b7311b
kandashi <jacobmpalmer@live.co.uk>
2021-08-05 10:54:48 +0100

    main

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,18 @@
-Hooks.once('init', async function() {
-
-});
 
 Hooks.once('ready', async function() {
+    libWrapper.register("faster-audio-preload", "Playlist.prototype._onSoundStart", earlyPreload, "OVERRIDE")
 
 });
+
+function earlyPreload(...args) {
+    let sound = args[0]
+    if ( ![CONST.PLAYLIST_MODES.SEQUENTIAL, CONST.PLAYLIST_MODES.SHUFFLE].includes(this.mode) ) return;
+    const apl = CONFIG.Playlist.autoPreloadSeconds;
+    if ( Number.isNumeric(apl) ) {
+      setTimeout(() => {
+        if ( !sound.playing ) return;
+        const next = this._getNextSound(sound.id);
+        if ( next ) next.sound.load();
+    }, (sound.sound.duration/2) * 1000);
+}
+  }
